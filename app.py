9ab3f60b1c03ffda69d3f65ee81614f8a3d6f1a2
SoularOfDawn <tntkala001@gmail.com>
2022-07-12 16:13:46 +0900

    커밋 테스트

--- app.py
+++ app.py
@@ -25,6 +25,29 @@ def detail():
 def main():
     return render_template("mainpage.html")
 
+@app.route('/park/<parkId>')
+def park(parkId):
+    return render_template("park.html", parkId=parkId)
+
+@app.route('/api/postMyRevies/<reviewId>', methods=['POST'])
+def postReview(reviewId):
+    currList = db.Reviews.find_one({'reviewId':reviewId})
+    parkId_receive = request.form['parkId_give']
+    date_receive = request.form['date_give']
+    rate_receive = request.form['rate_give']
+    weather_receive = request.form['weather_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'parkId': parkId_receive,
+        'finished_at': date_receive,
+        'rate': rate_receive,
+        'weather': weather_receive,
+        'comment': comment_receive
+    }
+    currList.reviews.insert_one(doc)
+    return jsonify({'msg': '리뷰 저장 완료'})
+
 @app.route('/header')
 def header():
     return render_template('header.html')
@@ -91,6 +114,28 @@ def getReviews(parkId):
     print(reviews)
 
     return render_template("mypage.html", user=user, parks=parks, currList=reviews, parkId=parkId)
+
+
+
+
+@app.route('/api/getParkInfo/<parkId>')
+def getParks(parkId):
+    currList = db.Parks.find_one({'parkId':parkId})
+    return jsonify({'Parks': currList})
+
+@app.route('/api/checkLogin')
+def checkLogin():
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
