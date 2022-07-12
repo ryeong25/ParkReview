6b2b1e26884a4c741e145619603b6b3e91ebc7ff
kimhyunjee <ysk30430@naver.com>
2022-07-12 15:26:23 +0900

    마이페이지+메인페이지 앱파이 초안

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# <<<<<<< HEAD
 from pymongo import MongoClient
 import jwt
 import datetime
@@ -13,17 +12,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
-
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
-db = client.parkReview
-
-# =======
-from select import select
-from flask import Flask, jsonify, render_template
-from pymongo import MongoClient
->>>>>>> ae8993430ec3f085aa55054aa20dd6f4b9ebc9aa
-
-app = Flask(__name__)
 
 client = MongoClient('mongodb+srv://Sparta:SpArTae8737be698@cluster0.licyu.mongodb.net/?retryWrites=true&w=majority')
 db = client.parkReview
@@ -78,7 +66,7 @@ def getParks(parkId):
 
     return render_template("mypage.html", user=user, parks=parks, currList=currList, parkId=parkId)
 
-<<<<<<< HEAD
+
 @app.route('/user/userId')
 def user(username):
     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
@@ -92,8 +80,6 @@ def user(username):
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
-=======
 # 나의 리뷰
 @app.route('/mypage/myReviews/<parkId>')
 def getReviews(parkId):
@@ -105,7 +91,6 @@ def getReviews(parkId):
     print(reviews)
 
     return render_template("mypage.html", user=user, parks=parks, currList=reviews, parkId=parkId)
->>>>>>> ae8993430ec3f085aa55054aa20dd6f4b9ebc9aa
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
