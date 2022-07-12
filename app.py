6b4c83a75267c9e762527a3b5ec971fd9906c702
kimhyunjee <ysk30430@naver.com>
2022-07-12 15:07:31 +0900

    app.py 초안

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+db = client.parkReview
 
 
 app = Flask(__name__)
@@ -20,6 +36,20 @@ def header():
 @app.route('/footer')
 def footer():
     return render_template('footer.html')
+
+@app.route('/user/userId')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
 
 
 if __name__ == '__main__':
