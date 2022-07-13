d0fd8c1f0a6427f4feba2efb1aafc701a1e62dca
SoularOfDawn <tntkala001@gmail.com>
2022-07-13 09:35:26 +0900

    공원페이지 리뉴얼

--- app.py
+++ app.py
@@ -55,19 +55,6 @@ def header():
 def footer():
     return render_template('footer.html')
 
-@app.route('/user/userId')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
 # 마이페이지
 @app.route('/mypage')
 def mypage():
